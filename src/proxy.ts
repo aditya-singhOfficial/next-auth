c90ce4a6c9dba8071ee0f311b2c5583b4012ddae
Aditya Singh <1211821022@ngpp.ac.in>
2026-02-02 23:15:06 +0530

    Add auth redirect logic for protected routes

---
+++ src/proxy.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function proxy(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path==='/' || path === '/login' || path === '/signup';
+
+  const token = request.cookies.get('token')?.value||'';
+
+  if(isPublicPath && token){
+    return NextResponse.redirect(new URL('/profile', request.url));
+  }
+
+  if(!isPublicPath && !token){
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+}
+ 
+export const config = {
+  matcher: [
+    '/',
+    '/login',
+    '/signup',
+    '/logout',
+    '/profile/:path*',
+  ],
+}
